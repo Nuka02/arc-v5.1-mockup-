9c15c8288bfaca62629536825c0a4e5989234ac1
Ajdin <ajdinnukic16@yahoo.com>
2019-01-14 11:19:31 +0100

    hj

--- app/src/main/java/com/arcdrive/arc/ApplicationActivity.java
+++ app/src/main/java/com/arcdrive/arc/ApplicationActivity.java
@@ -2,12 +2,29 @@ package com.arcdrive.arc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ApplicationActivity extends AppCompatActivity {
+
+    private Button callRide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
+
+        callRide = findViewById(R.id.button5);
+
+        callRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    void sendSignal() {
+        
     }
 }
